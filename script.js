2f7452635b67e6e64152e9c19a0e9bf3d0b54ba8
SriMoorthy <srimoorthy27@gmail.com>
2024-06-17 11:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function firstWord(s) {
+function firstWord('see and stop') {
   // your code here
+	let S = s.trim()
+	let ans = ''
+	for (let i = 0; i < S.length; i++) {
+		if(S[i]==' '){
+			break
+		}
+		else{
+			ans+=S[i]
+		}
+	}
+	return ans
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const  = prompt("Enter String:");
+// alert(firstWord(s));
